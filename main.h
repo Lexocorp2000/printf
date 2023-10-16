01dcd603cbe30e418ff733236502c059522a6a34
AshTechs <ashbelh@gmail.com>
2023-10-16 07:47:21 -0700

    Sixth

--- main.h
+++ main.h
@@ -13,7 +13,6 @@
  * struct format - match the conversion specifiers for printf
  * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
  * @f: type pointer to function for the conversion specifier
- *
  */
 
 typedef struct format
@@ -36,9 +35,7 @@ int print_rot13(va_list val);
 int print_int(va_list val);
 int print_dec(va_list val);
 int _strlen(char *str);
-
 int _strlenc(const char *str);
-
 int print_37(void);
 int print_char(va_list val);
 int print_string(va_list val);
